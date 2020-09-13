fdb337a5d7c13051cc1dd008c83c8ed2a28d9b68
晚安北京 <120907030@qq.com>
2020-09-13 16:49:47 +0800

    点击追加当前选中样式

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div
+      v-for="(item,index) in message"
+      :key="index"
+      :class="{active:currentIndex===index}"
+      @click="itemClick(index)"
+    >{{item}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentIndex: 0,
+      message: ["哈哈哈", "啦啦啦", "呵呵呵"],
+    };
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index;
+    },
+  },
+};
+</script>
+
+<style>
+.active {
+  color: red;
+}
+</style>
